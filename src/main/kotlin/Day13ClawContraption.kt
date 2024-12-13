f7e3ab810519ac742f523a105fc087cbe0130aab
Denec <dzenevich@mail.ru>
2024-12-13 20:39:56 +0300

    optimize day 13 solution

--- src/main/kotlin/Day13ClawContraption.kt
+++ src/main/kotlin/Day13ClawContraption.kt
@@ -12,16 +12,19 @@ class Day13ClawContraption {
         val buttonA = Pair(input[0].split(" ")[2].substring(2).dropLast(1).toInt(), input[0].split(" ")[3].substring(2).toInt())
         val buttonB = Pair(input[1].split(" ")[2].substring(2).dropLast(1).toInt(), input[1].split(" ")[3].substring(2).toInt())
         val prize = Pair(input[2].split(" ")[1].substring(2).dropLast(1).toInt(), input[2].split(" ")[2].substring(2).toInt())
-        var tokens = 1000
-        for (bCount in 0..100) {
-            for (aCount in 0..100) {
-                if (buttonB.first * bCount + buttonA.first * aCount == prize.first
-                    && buttonB.second * bCount + buttonA.second * aCount == prize.second) {
-                    tokens = tokens.coerceAtMost(bCount + aCount * 3)
-                }
-            }
-        }
 
-        return if (tokens == 1000) 0 else tokens
+        val delta = buttonA.first * buttonB.second - buttonA.second * buttonB.first
+
+        val deltaX = prize.first * buttonB.second - prize.second * buttonB.first
+
+        val deltaY = buttonA.first * prize.second - buttonA.second * prize.first
+
+        if (deltaX % delta != 0 || deltaY % delta != 0) return 0
+
+        val x = deltaX / delta
+        val y = deltaY / delta
+        if (x < 0 || y < 0) return 0
+
+        return 3 * x + y
     }
 }
